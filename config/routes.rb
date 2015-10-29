6972c58eca6703e9fb96685b3c79b0988eb8c2a5
Yuji Goto <yuji1129xxx@gmail.com>
2015-10-29 18:57:55 +0900

    add devise login session
    deviseでログイン機能を実装した

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'top#index'
+  devise_for :users, :controllers => {
+    confirmations: "users/confirmations",
+    # omniauth_callbacks: "users/omniauth_callbacks",
+    passwords: "users/passwords",
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    # unlocks: "users/unlocks",
+  }
 end
